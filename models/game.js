cb2202d6f075ece6bc41787d659a0f3a123a97ec
Kolya Venturi <kolya.venturi@enx3s.com>
2018-09-10 15:03:52 -0600

    Game IDs

--- models/game.js
+++ models/game.js
@@ -1,3 +1,9 @@
 export default class Game {
+  constructor() {
+    this._id = Math.floor(Math.random() * 1e5);
+  }
 
+  get id() {
+    return this._id;
+  }
 }
--- spec/models/game.spec.js
+++ spec/models/game.spec.js
@@ -1,4 +1,4 @@
-import Game from '../../models/Game';
+import Game from '../../models/game';
 
 describe('Game', () => {
   let game = null;
