169bf354e8f358985f59d71278c1c2c7f9e8ecdd
Kolya Venturi <kolya.venturi@enx3s.com>
2018-09-10 15:25:04 -0600

    Player can have a name

--- models/player.js
+++ models/player.js
@@ -1,9 +1,23 @@
+import Chance from 'chance';
+
+const chance = new Chance();
+
 export default class Player {
   constructor(id) {
     this._id = id;
+    this._name = chance.name({middle: true, prefix: true});
   }
 
   get id() {
     return this._id;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+    return this._name;
   }
 }
--- spec/models/player.spec.js
+++ spec/models/player.spec.js
@@ -12,4 +12,20 @@ describe('Player', () => {
 
     expect(player.id).toEqual(expect.any(String));
   });
+
+  test('it has a default name', () => {
+    expect(player.name).not.toBeNull();
+
+    expect(player.name).toEqual(expect.any(String));
+  });
+
+  test('its name can change', () => {
+    const newName = 'JohnDoe';
+
+    expect(player.name).not.toEqual(newName);
+
+    player.name = newName;
+
+    expect(player.name).toEqual(newName);
+  });
 });
