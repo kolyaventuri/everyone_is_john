dda7765cb405489e54935c836bfa7a549e05f1b5
Kolya Venturi <kolya.venturi@enx3s.com>
2018-09-13 17:29:38 -0600

    Start/join game buttons

--- spec/components/routes/home.spec.js
+++ spec/components/routes/home.spec.js
@@ -1,19 +1,35 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 
+import {Link} from 'react-router-dom';
+
 import Home from '../../../src/components/routes/home.jsx';
 import Rules from '../../../src/components/routes/components/rules.jsx';
 
 describe('<Home />', () => {
   let home = null;
+  let links = null;
 
   beforeAll(() => {
     home = shallow(<Home/>);
+    links = [...home.find(Link)];
   });
 
   test('should render the rules', () => {
     const rules = [...home.find(Rules)];
 
     expect(rules).toHaveLength(1);
+  });
+
+  test('should render a start game button', () => {
+    const startGameButton = links.filter(({props}) => props.to === '/game/new');
+
+    expect(startGameButton).toHaveLength(1);
+  });
+
+  test('should render a join game button', () => {
+    const joinGameButton = links.filter(({props}) => props.to === '/game/join');
+
+    expect(joinGameButton).toHaveLength(1);
   });
 });
--- src/components/routes/home.jsx
+++ src/components/routes/home.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 import Rules from './components/rules.jsx';
 
 const Home = () => (
   <div>
+    <div>
+      <div className="button">
+        <Link to="/game/new">Start a Game</Link>
+      </div>
+      <div className="button">
+        <Link to="/game/join">Join a Game</Link>
+      </div>
+    </div>
     <Rules/>
   </div>
 );
