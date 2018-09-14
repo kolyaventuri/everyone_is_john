9f299674c5f6a989867a3fdf0ec41891eedf69d9
Kolya Venturi <kolya.venturi@enx3s.com>
2018-09-13 21:40:45 -0600

    Add SocketEvents class

---
+++ src/lib/socket-events.js
@@ -0,0 +1,9 @@
+export default class SocketEvents {
+  constructor(gm) {
+    this.GameManager = gm;
+  }
+
+  initGame(data) {
+    window.location = `/game/${data.id}`;
+  }
+}
--- src/lib/socket-handler.js
+++ src/lib/socket-handler.js
@@ -1,11 +1,21 @@
+import SocketEvents from './socket-events';
+
 class SocketHandler {
   constructor(socket, gm) {
     this.socket = socket;
-    this.gm = gm;
+    this.event = new SocketEvents(gm);
+
+    this.registerEvents();
   }
 
   static register(socket, gm) {
     (() => new this(socket, gm))();
+  }
+
+  registerEvents() {
+    const {socket, event} = this;
+
+    socket.on('game.initiate', event.initGame);
   }
 }
 
