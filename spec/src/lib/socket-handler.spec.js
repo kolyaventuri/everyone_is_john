db68ac3de5af3907dc6479e1af51852869937d8d
Kolya Venturi <kolya.venturi@enx3s.com>
2018-09-16 11:48:30 -0600

    Unit test more socket events

--- spec/src/lib/socket-handler.spec.js
+++ spec/src/lib/socket-handler.spec.js
@@ -2,11 +2,21 @@ import SocketHandler from '../../../src/lib/socket-handler';
 import socket from '../../helpers/mock-socket';
 
 const mockInitPlayer = jest.fn();
+const mockInitGame = jest.fn();
+const mockRejectInit = jest.fn();
 
 jest.mock('../../../src/lib/socket-events', () => {
   return class SocketEvents {
     initPlayer(socket) {
       mockInitPlayer(socket);
+    }
+
+    initGame(id) {
+      mockInitGame(id);
+    }
+
+    rejectInitGame() {
+      mockRejectInit();
     }
   };
 });
@@ -18,5 +28,20 @@ describe('SocketHandler', () => {
     socket._trigger('turtle');
 
     expect(mockInitPlayer).toHaveBeenCalledWith(socket);
+  });
+
+  test('triggers game.initiate with an ID', () => {
+    const id = 'ABCDE';
+
+    socket._trigger('game.initiate', id);
+
+    expect(mockInitGame).toHaveBeenCalledWith(id);
+  });
+
+  test('triggers game.initiate.reject', () => {
+    socket._trigger('game.initiate.reject');
+
+    expect(mockRejectInit).toHaveBeenCalled();
+    expect(mockInitGame).not.toHaveBeenCalled();
   });
 });
