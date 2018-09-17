d9dd0066823025fc449601f0fa5a82e68073318d
Kolya Venturi <kolya.venturi@enx3s.com>
2018-09-17 11:06:53 -0600

    Modify mock socket to function more like rooms actually do

--- spec/helpers/mock-socket.js
+++ spec/helpers/mock-socket.js
@@ -2,13 +2,16 @@ class MockSocket {
   emit = jest.fn()
 
   join = jest.fn(room => {
-    this.rooms[room] = this.rooms[room] || [];
-    this.rooms[room].push(this);
+    this.roomClients[room] = this.roomClients[room] || [];
+    this.roomClients[room].push(this);
+    this.rooms[room] = room;
   })
 
   events = {}
 
   rooms = {}
+
+  roomClients = {}
 
   // eslint-disable-next-line no-useless-constructor
   constructor() {}
@@ -16,7 +19,7 @@ class MockSocket {
   in = name => {
     return {
       emit: (event, data) => {
-        for (const client of this.rooms[name]) {
+        for (const client of this.roomClients[name]) {
           client.emit(event, data);
         }
       }
