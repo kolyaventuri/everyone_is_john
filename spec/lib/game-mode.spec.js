383f30b9af098e7dadb75e87cd471f1759a369a8
Kolya Venturi <kolya.venturi@enx3s.com>
2018-10-06 06:44:11 -0700

    Add spec for GameMode join locking

---
+++ spec/lib/game-mode.spec.js
@@ -0,0 +1,57 @@
+import Game from '../../models/game';
+import Player from '../../models/player';
+import GameMode from '../../lib/game-mode';
+import MockSocket from '../helpers/mock-socket';
+import repos from '../../services/repositories';
+
+const {playerRepository, gameRepository} = repos;
+
+describe('GameMode', () => {
+  let game = null;
+  let owner = null;
+  let mockIo = null;
+
+  beforeEach(() => {
+    playerRepository.clear();
+    gameRepository.clear();
+
+    owner = new Player(new MockSocket(), 'id');
+
+    mockIo = new MockSocket();
+
+    game = new Game(mockIo, owner);
+  });
+
+  test('SETUP allows users to join the game', () => {
+    game.mode = GameMode.SETUP;
+
+    const player = new Player(new MockSocket(), '1');
+
+    const result = game.addPlayer(player);
+
+    expect(result).toBeTruthy();
+    expect(game.players).toHaveLength(1);
+  });
+
+  test('Non-SETUP disallows users to join', () => {
+    const player = new Player(new MockSocket(), '1');
+
+    // VOTING mode
+    game.mode = GameMode.VOTING;
+    const resultVoting = game.addPlayer(player);
+    expect(resultVoting).toBeFalsy();
+    expect(game.players).toHaveLength(0);
+
+    // PLAYING mode
+    game.mode = GameMode.PLAYING;
+    const resultPlaying = game.addPlayer(player);
+    expect(resultPlaying).toBeFalsy();
+    expect(game.players).toHaveLength(0);
+
+    // LOCKED mode
+    game.mode = GameMode.LOCKED;
+    const resultLocked = game.addPlayer(player);
+    expect(resultLocked).toBeFalsy();
+    expect(game.players).toHaveLength(0);
+  });
+});
