95d0edecebee01f95d1e75e4f80cb746a291f50f
Kolya Venturi <kolya.venturi@enx3s.com>
2018-10-07 07:52:01 -0700

    Refactor tests to group them better

--- spec/lib/game-mode.spec.js
+++ spec/lib/game-mode.spec.js
@@ -26,81 +26,96 @@ describe('GameMode', () => {
     game = new Game(mockIo, owner);
   });
 
-  test('SETUP allows users to join the game', () => {
-    game.mode = SETUP;
+  describe('Joining', () => {
+    test('is possible in SETUP state', () => {
+      game.mode = SETUP;
 
-    const result = game.addPlayer(player);
+      const result = game.addPlayer(player);
 
-    expect(result).toBeTruthy();
-    expect(game.players).toHaveLength(1);
+      expect(result).toBeTruthy();
+      expect(game.players).toHaveLength(1);
+    });
+
+    test('is not possible in non-SETUP states', () => {
+      // VOTING mode
+      game.mode = VOTING;
+      const resultVoting = game.addPlayer(player);
+      expect(resultVoting).toBeFalsy();
+      expect(game.players).toHaveLength(0);
+
+      // PLAYING mode
+      game.mode = PLAYING;
+      const resultPlaying = game.addPlayer(player);
+      expect(resultPlaying).toBeFalsy();
+      expect(game.players).toHaveLength(0);
+
+      // LOCKED mode
+      game.mode = LOCKED;
+      const resultLocked = game.addPlayer(player);
+      expect(resultLocked).toBeFalsy();
+      expect(game.players).toHaveLength(0);
+    });
   });
 
-  test('Non-SETUP disallows users to join', () => {
-    // VOTING mode
-    game.mode = VOTING;
-    const resultVoting = game.addPlayer(player);
-    expect(resultVoting).toBeFalsy();
-    expect(game.players).toHaveLength(0);
+  describe('Bidding', () => {
+    test('activates a new auction upon VOTING state', () => {
+      game.mode = LOCKED;
 
-    // PLAYING mode
-    game.mode = PLAYING;
-    const resultPlaying = game.addPlayer(player);
-    expect(resultPlaying).toBeFalsy();
-    expect(game.players).toHaveLength(0);
+      expect(game.auction).toBeNull();
 
-    // LOCKED mode
-    game.mode = LOCKED;
-    const resultLocked = game.addPlayer(player);
-    expect(resultLocked).toBeFalsy();
-    expect(game.players).toHaveLength(0);
+      game.mode = VOTING;
+
+      expect(game.auction).toBeInstanceOf(Auction);
+    });
+
+    test('is allowed in VOTING state', () => {
+      game.addPlayer(player);
+      game.mode = VOTING;
+
+      const result = game.addBid(player, 1);
+
+      expect(result).toEqual(1);
+    });
+
+    test('fails if willpower out of bounds', () => {
+      game.addPlayer(player);
+      game.mode = VOTING;
+
+      const result = game.addBid(player, player.stats.willpower + 1);
+      expect(result).toEqual(false);
+
+      const resultNegative = game.addBid(player, -1);
+      expect(resultNegative).toEqual(false);
+    });
+
+    test('not allowed in non-VOTING states', () => {
+      game.addPlayer(player);
+
+      // SETUP
+      game.mode = SETUP;
+      const resultSetup = game.addBid(player, 1);
+      expect(resultSetup).toEqual(false);
+
+      // PLAYING
+      game.mode = PLAYING;
+      const resultPlaying = game.addBid(player, 1);
+      expect(resultPlaying).toEqual(false);
+
+      // LOCKED
+      game.mode = LOCKED;
+      const resultLocked = game.addBid(player, 1);
+      expect(resultLocked).toEqual(false);
+    });
   });
 
-  test('Activating VOTING state starts new auction', () => {
-    game.mode = LOCKED;
+  describe('Goals', () => {
+    test('can be set during SETUP state', () => {
+      game.addPlayer(player);
+      game.mode = SETUP;
 
-    expect(game.auction).toBeNull();
+      const result = player.setGoal('Random goal.');
 
-    game.mode = VOTING;
-
-    expect(game.auction).toBeInstanceOf(Auction);
-  });
-
-  test('Bidding is allowed in VOTING state', () => {
-    game.addPlayer(player);
-    game.mode = VOTING;
-
-    const result = game.addBid(player, 1);
-
-    expect(result).toEqual(1);
-  });
-
-  test('game.addBid returns false if bid fails', () => {
-    game.addPlayer(player);
-    game.mode = VOTING;
-
-    const result = game.addBid(player, player.stats.willpower + 1);
-    expect(result).toEqual(false);
-
-    const resultNegative = game.addBid(player, -1);
-    expect(resultNegative).toEqual(false);
-  });
-
-  test('Bidding is NOT allowed in other states', () => {
-    game.addPlayer(player);
-
-    // SETUP
-    game.mode = SETUP;
-    const resultSetup = game.addBid(player, 1);
-    expect(resultSetup).toEqual(false);
-
-    // PLAYING
-    game.mode = PLAYING;
-    const resultPlaying = game.addBid(player, 1);
-    expect(resultPlaying).toEqual(false);
-
-    // LOCKED
-    game.mode = LOCKED;
-    const resultLocked = game.addBid(player, 1);
-    expect(resultLocked).toEqual(false);
+      expect(result).toBeTruthy();
+    });
   });
 });
